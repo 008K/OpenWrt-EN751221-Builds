1a4a951a221020199828d02f48729190386a6cd4
008K <67450505+008K@users.noreply.github.com>
2026-08-05 09:23:30 +0800

    Update en751221.mk

--- en751221.mk
+++ en751221.mk
@@ -98,9 +98,8 @@ define Device/cmhk_gs2210
   DEVICE_MODEL := GS2210
   DEVICE_DTS := en751221_cmhk_gs2210
   SUPPORTED_DEVICES := cmhk,gs2210
-  KERNEL := kernel-bin | append-dtb | lzma 
   IMAGES := tclinux.trx
-  IMAGE/tclinux.trx := append-kernel | tclinux-trx
+  IMAGE/tclinux.trx := append-kernel | lzma | tclinux-trx
   DEVICE_PACKAGES := kmod-usb3 kmod-mt7603 kmod-mt76x2
 endef
 TARGET_DEVICES += cmhk_gs2210
